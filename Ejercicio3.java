fcbb78ba7dfc5bb8568e4d0f9c8da80769b7b7d7
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-05 12:53:23 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3{
+    public static void main(String [] args){
+        System.out.printf("%-10s \n ","Computer"+"Ordenador");
+        System.out.printf("%-10s \n ","Teclado"+"Keyboard");
+        System.out.printf("%-10s \n ","Raton"+"Mouse");
+        System.out.printf("%-10s \n ","Movil"+"Phone");
+        System.out.printf("%-10s \n ","Foto"+"Photo");
+        System.out.printf("%-10s \n ","Placa Base"+"Mother Board");
+        System.out.printf("%-10s \n ",""+"Ordenador");
+        System.out.printf("%-10s \n ","Computer"+"Ordenador");
+        System.out.printf("%-10s \n ","Computer"+"Ordenador");
+        System.out.printf("%-10s \n ","Computer"+"Ordenador");
+    }
+}
