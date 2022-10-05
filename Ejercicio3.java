2e8c53c7a189bb97accf5d64eea36f0367719a4d
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-05 13:13:15 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,14 +1,15 @@
 public class Ejercicio3{
     public static void main(String [] args){
-        System.out.printf("%-10s \n ","Computer"+"Ordenador");
-        System.out.printf("%-10s \n ","Teclado"+"Keyboard");
-        System.out.printf("%-10s \n ","Raton"+"Mouse");
-        System.out.printf("%-10s \n ","Movil"+"Phone");
-        System.out.printf("%-10s \n ","Foto"+"Photo");
-        System.out.printf("%-10s \n ","Placa Base"+"Mother Board");
-        System.out.printf("%-10s \n ",""+"Ordenador");
-        System.out.printf("%-10s \n ","Computer"+"Ordenador");
-        System.out.printf("%-10s \n ","Computer"+"Ordenador");
-        System.out.printf("%-10s \n ","Computer"+"Ordenador");
+        /*The f in print, put a format for the print, and the %-20s put a space from the first to the second wit the %s and \n*/
+        System.out.printf("%-20s %s\n", "Computer", "Ordenador");
+        System.out.printf("%-20s %s\n ", "Teclado", "Keyboard");
+        System.out.printf("%-20s %s\n ", "Raton", "Mouse");
+        System.out.printf("%-20s %s\n ", "Movil", "Phone");
+        System.out.printf("%-20s %s\n ", "Foto", "Photo");
+        System.out.printf("%-20s %s\n ", "Placa Base", "Mother Board");
+        System.out.printf("%-20s %s\n ", "Computer", "Ordenador");
+        System.out.printf("%-20s %s\n ", "Computer", "Ordenador");
+        System.out.printf("%-20s %s\n ",  "Computer", "Ordenador");
+        System.out.printf("%-20s %s\n ",  "Computer", "Ordenador");
     }
 }
