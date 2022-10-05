07fdf2ead73085f2cc1e3580ce2a0a148bbbb2ec
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-05 12:31:07 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2{
+    public static void main(String [] args){
+        System.out.print("Pablo García Muñoz");
+        System.out.print("Dirección: Avenida España nº13");
+        System.out.print("Num telf: 000000000");
+    }
+}
