520337175cd228707f7336f34f510f1a18bdcdda
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-06 10:53:59 +0200

    Ejercicio10

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,12 +1,18 @@
 public class Ejercicio10{
     public static void main (String [] args){
-        System.out.println("  _______");
+        System.out.printf("\033[37m");
+        System.out.println("  _______ ");
+        System.out.printf("\033[35m");
         System.out.println(" |.-----.|");
         System.out.println(" ||     ||");
+        System.out.println(" ||_____||");
         System.out.println(" | .     |");
+        System.out.printf("\033[33m");
         System.out.println(" |-|-  oo|");
+        System.out.printf("\033[35m");
         System.out.println(" |  _ _  |");
         System.out.println(" |       /");
+        System.out.printf("\033[37m");
         System.out.println(" ========");
     }
 }
