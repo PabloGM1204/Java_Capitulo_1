5143d53ef6646868fbe2d3f7b7af83da220360de
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-06 10:01:05 +0200

    Ejercicio6

---
+++ Ejericicio6.java
@@ -0,0 +1,9 @@
+public class Ejericicio6{
+    public static void main(String [] args){
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+    }
+}
