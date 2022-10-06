e05a7dee10ba9a3f86ad2b14863f7de4d7b46c73
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-06 10:32:59 +0200

    Ejercicio9

---
+++ Ejercicio9.java
@@ -0,0 +1,12 @@
+public class Ejercicio9{
+    public static void main (String [] args){
+        System.out.println("  _______");
+        System.out.println(" |.-----.|");
+        System.out.println(" ||     ||");
+        System.out.println(" | .     |");
+        System.out.println(" |-|-  oo|");
+        System.out.println(" |  _ _  |");
+        System.out.println(" |       /");
+        System.out.println(" ========");
+    }
+}
