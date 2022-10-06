3a2dc5a5601700c96951d7e66713d2df924ac1d9
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-06 08:59:27 +0200

    Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,13 @@
+public class Ejercicio4{
+    public static void main(String [] args){
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes");
+        System.out.printf("%s\n","==========================================");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "PROG", "SISIN", "ENTD", "PROG", "FOL");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "PROG", "SISIN", "ENTD", "PROG", "FOL");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "LENM", "SISIN", "PROG", "LENM", "FOL");
+        System.out.printf("%s\n", "==================RECREO==================");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "LENM", "PROG", "PROG", "LENM", "SISIN");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "BBDD", "PROG", "BBDD", "BBDD", "SISIN");
+        System.out.printf("%-7s %-7s %-10s %-7s %-7s\n", "BBDD", "ENTD", "BBDD", "BBDD", "SISIN");
+    }
+}
